c5dfcdc1a3947b42ba9efd6f3a58247c4a05eb0e
Ethan Kovalenko <ashervoronin@gmail.com>
2021-10-26 02:36:51 -0500

    Created game window and added constants.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+import pygame
+
+# Game window constants
+WIDTH = 1200
+HEIGHT = 720
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Color constants
+WHITE = (255, 255, 255)
+CYAN = (0, 255, 255)
--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 import pygame
+from pygame import constants
+from pygame.constants import K_ESCAPE, KEYDOWN
+import constants
+
+# Game window
+pygame.display.set_caption('Scrolling Platformer')
 
 
-class Main:
+class Main():
     def __init__(self):
-        pass
+        # Frames
+        self.clock = pygame.time.Clock()
+        self.frames_per_second = 60
+
+        # Game variables
 
     def run(self):
-        pass
+        running = True
+
+        # Game loop
+        while running == True:
+
+            # Game frames per second
+            self.clock.tick(self.frames_per_second)
+
+            # Exit game
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                # Escape key
+                elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                        return
+
+            # Game window
+            constants.SCREEN.fill(constants.CYAN)
+
+            # Display the screen
+            pygame.display.flip()
+
+
+# Run only main class
+if __name__ == '__main__':
+    Main().run()
